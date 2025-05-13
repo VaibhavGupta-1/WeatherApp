82d4109d9ff1f4716ac8164de2ab535d53aabd1a
Studggle <vg1992005@gmail.com>
2025-05-14 00:01:41 +0530

    .

--- app/src/main/java/com/example/weatherapp/MainActivity.kt
+++ app/src/main/java/com/example/weatherapp/MainActivity.kt
@@ -11,16 +11,20 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.lifecycle.ViewModelProvider
 import com.example.weatherapp.ui.theme.WeatherAppTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        val weatherViewModel= ViewModelProvider(this).get(WeatherViewModel::class.java)
+
         enableEdgeToEdge()
         setContent {
             WeatherAppTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-
+                    WeatherPage(weatherViewModel)
                 }
             }
         }
--- app/src/main/java/com/example/weatherapp/WeatherPage.kt
+++ app/src/main/java/com/example/weatherapp/WeatherPage.kt
@@ -1,7 +1,13 @@
 package com.example.weatherapp
 
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Search
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
 import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
@@ -9,15 +15,26 @@ import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
 
 @Composable
-fun WeatherPage() {
-    var city by remember { mutableStateOf("") }
+fun WeatherPage(viewModel: WeatherViewModel) {
+    var city by remember { mutableStateOf("")
+    }
 
-    Column {
-        Row {
+    Column (
+        modifier = Modifier.padding(8.dp),
+        horizontalAlignment = Alignment.CenterHorizontally
+    ){
+        Row (
+            modifier = Modifier.padding(8.dp),
+            verticalAlignment = Alignment.CenterVertically,
+            horizontalArrangement = Arrangement.SpaceEvenly
+        ){
             OutlinedTextField(
+                modifier = Modifier.weight(1f),
                 value =city ,
                 onValueChange ={
                 city = it
@@ -26,6 +43,9 @@ fun WeatherPage() {
                     Text("Search City")
                         },
             )
+            IconButton(onClick = {viewModel.getData(city)}) {
+                Icon(imageVector = Icons.Default.Search, contentDescription = "Search for any location")
+            }
         }
     }
 }
