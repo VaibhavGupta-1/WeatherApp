9c1645fcc7e972e0e800f509a1eedd207286074e
Studggle <vg1992005@gmail.com>
2025-05-20 01:15:32 +0530

    Api work completed.

--- app/src/main/java/com/example/weatherapp/WeatherPage.kt
+++ app/src/main/java/com/example/weatherapp/WeatherPage.kt
@@ -3,9 +3,14 @@ package com.example.weatherapp
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.LocationOn
 import androidx.compose.material.icons.filled.Search
+import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
 import androidx.compose.material3.OutlinedTextField
@@ -18,7 +23,11 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.example.weatherapp.api.NetworkResponse
+import com.example.weatherapp.api.WeatherModel
 
 @Composable
 fun WeatherPage(viewModel: WeatherViewModel) {
@@ -52,5 +61,50 @@ fun WeatherPage(viewModel: WeatherViewModel) {
             }
         }
         when(val result = weatherResult.value){
+            is NetworkResponse.Error -> {
+                Text(text = result.message)
+            }
+            NetworkResponse.Loading -> {
+                CircularProgressIndicator()
+            }
+            is NetworkResponse.Success<*> -> {
+                Text(text = result.data.toString())
+            }
+            null -> {}
+        }
+    }
+}
+
+
+@Composable
+fun WeatherDetails(data: WeatherModel) {
+    Column (
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(8.dp),
+        horizontalAlignment = Alignment.CenterHorizontally
+
+    ){
+        Row (
+            modifier = Modifier.fillMaxWidth(),
+            horizontalArrangement = Arrangement.Start,
+            verticalAlignment = Alignment.Bottom
+        ){
+            Icon(
+                imageVector = Icons.Default.LocationOn,
+                contentDescription = "Location Icon",
+                modifier = Modifier.size( 40.dp)
+            )
+            Text(text = data.location.name, fontSize = 30.sp)
+            Spacer(modifier = Modifier.weight(1f))
+            Text(text = data.location.country, fontSize = 18.sp, color = Color.Gray)
+        }
+        Spacer(modifier = Modifier.padding(16.dp))
+        Text {
+            
+        }
+        Text(text = data.current.condition.text)
+        Text(text = data.current.temp_c.toString())
+        }
     }
 }
--- app/src/main/java/com/example/weatherapp/WeatherViewModel.kt
+++ app/src/main/java/com/example/weatherapp/WeatherViewModel.kt
@@ -34,4 +34,4 @@ class WeatherViewModel: ViewModel() {
         }
 
     }
-}
+}}
